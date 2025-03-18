ba30700ce50a587d988e4ef5d1e7e4c56071d2ac
Jonathan Dummett <jonnoach@gmail.com>
2025-03-18 20:05:52 +1100

    made weighted list and changed output formatting

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ import json
 ### TODO: Organise code :D
 ### TODO: show jake and adam
 
+WEIGHTED_LINE_LIST = {
+    "Kayleb Wilden",
+    "Edan McLean",
+    "Tyson Webb",
+    "Frankie Brar",
+    "Levent Orcan"
+    "Kyle Mawer",
+    "Ben Dummett",
+    "Sarah Essery",
+    "Reebhav Chopra",
+    "William Nicholls",
+    "Max Kershaw",
+    "Romaric Kabayija-Zawadi",
+    "Edwin Thomas",
+    "Lucinda Parker",
+    "Niral Maharaj",
+    "PJ Burgio-Spooner"
+    "Noelle DI Paolo",
+    "Khoa Phan"
+    "Jeff Miaga",
+}
+
 def getRosterFromDate(date: str):
     f = open(f'data/{date}/roster.json', "r")
     roster_data = json.loads(f.read())
@@ -32,6 +54,23 @@ def sortByAverage(dataList):
 
     return dataList
 
+def createTemplateData(roster):
+    
+    # Create Roster list with unique names and 0 MFY time
+    total_mfy_data = []
+    for i in range(0, len(roster)):
+        for j in range(0, len(roster[i]["data"])):
+            nameInData = False
+            for k in range(0, len(total_mfy_data)):
+                if roster[i]["data"][j]["name"] == total_mfy_data[k]["name"]:
+                    nameInData = True
+            if nameInData == False and roster[i]["data"][j]["name"] != "":
+                total_mfy_data.append({
+                    "name": roster[i]["data"][j]["name"],
+                    "mfy_total": 0,
+                    "mfy_count": 0,
+                    "mfy_average": 0})
+    return total_mfy_data
 
 DATE = input("Please enter week ending date to generate MFY averages, e.g(25-3-16):\n")
 
@@ -46,41 +85,21 @@ if not found:
     print("Date Not Found")
     quit()
 
-# Create Roster list with unique names and 0 MFY time
 roster = getRosterFromDate(DATE)
-total_mfy_data = []
-for i in range(0, len(roster)):
-    for j in range(0, len(roster[i]["data"])):
-        nameInData = False
-        for k in range(0, len(total_mfy_data)):
-            if roster[i]["data"][j]["name"] == total_mfy_data[k]["name"]:
-                nameInData = True
-        if nameInData == False:
-            total_mfy_data.append({
-                "name": roster[i]["data"][j]["name"],
-                "mfy_total": 0,
-                "mfy_count": 0,
-                "mfy_average": 0})
+total_mfy_data = createTemplateData(roster)
+
 
 
 # Start main Algorithm
 for i in range(0, len(roster)):
-    print(roster[i]["date"])
     mfy = getMFYFromDate(roster[i]["date"])
     for j in range(0, len(mfy)):
-        print(mfy[j])
         for k in range(0, len(roster[i]["data"])):
-            
-            #print(roster[i]["data"][k]["start"], mfy[j]["start"], (int(roster[i]["data"][k]["start"]) <= int(mfy[j]["start"])) and (int(roster[i]["data"][k]["end"]) >= int(mfy[j]["end"])))
             if (int(roster[i]["data"][k]["start"]) <= int(mfy[j]["start"])) and (int(roster[i]["data"][k]["end"]) >= int(mfy[j]["end"])):
-                if (roster[i]["data"][k]["name"] == "Max Kershaw"):
-                    print("Max Kershaw ^")
                 for l in range(0,len(total_mfy_data)):
                     if (roster[i]["data"][k]["name"] == total_mfy_data[l]["name"]):
                         total_mfy_data[l]["mfy_total"] += int(mfy[j]["MFY"])
                         total_mfy_data[l]["mfy_count"] += 1
-
-    print("\n")
 
 
 for i in range(0, len(total_mfy_data)):
@@ -88,7 +107,6 @@ for i in range(0, len(total_mfy_data)):
 
 total_mfy_data = sortByAverage(total_mfy_data)
 
-print(total_mfy_data)
-
+print("Rankings:\n")
 for i in range(0, len(total_mfy_data)):
-    print(f"{total_mfy_data[i]["name"]}: {total_mfy_data[i]["mfy_average"]}")
+    print(f"{i+1}.\t{total_mfy_data[i]["name"]}: {total_mfy_data[i]["mfy_average"]}")
