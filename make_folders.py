d4160d4342bfefccce4b38628fa9647c12ad3819
Jonathan Dummett <jonnoach@gmail.com>
2025-03-18 12:36:46 +1100

    changed folder creation, removed fake data

--- make_folders.py
+++ make_folders.py
@@ -1,21 +1,23 @@
 import os
 
-
-
 days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-start_date = [13, 3] # dd/mm
-end_date = [18, 3]   # dd/mm
-# NOTE this program will create folders TO end_date - 1
+start_date = [16, 3] # dd/mm
+add_weeks = 1
+
+def get_next_date(last_date: str) -> str:
+    [year, month, day] = last_date.split('-')
+    year, month, day = int(year), int(month), int(day)
+    day += 7
+    if day > days_in_month[month-1]:
+        day -= days_in_month[month-1]
+        month += 1
+    if month > 12:
+        month = 1
+        year += 1
+    return f"{year}-{month}-{day}"
 
 
-
-curr_date = start_date
-while curr_date != end_date:
-    string = f"data/{start_date[0]}-{start_date[1]}"
-    os.makedirs(string)
-    # print(string)
-    curr_date[0] += 1
-    if curr_date[0] > days_in_month[curr_date[1] - 1]:
-        curr_date[0] = 1
-        curr_date[1] += 1
+list = os.listdir('data')
+list.sort()
+os.makedirs(f"data/{get_next_date(list[-1])}")
